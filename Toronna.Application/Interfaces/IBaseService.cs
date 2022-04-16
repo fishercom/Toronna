3257e286b91e366cd7afe33431110389d10b959a
Fischer Tirado <fishdev@gmail.com>
2022-04-15 23:24:37 -0500

    services

---
+++ Toronna.Application/Interfaces/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Toronna.Domain.Interfaces.Repositories;
+
+namespace Toronna.Application.Interfaces;
+
+public interface IBaseService<TEntity, TEntityId>
+    : IBaseRepository<TEntity, TEntityId>
+{
+
+}
+
---
+++ Toronna.Application/Services/SchemaService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Toronna.Application.Interfaces;
+using Toronna.Domain.Entities;
+using Toronna.Domain.Interfaces.Repositories;
+
+namespace Toronna.Application.Services;
+
+public class SchemaService : IBaseService<Schema, long>
+{
+    private readonly IBaseRepository<Schema, long> repository;
+    public SchemaService(IBaseRepository<Schema, long> _repository)
+    {
+        repository = _repository;
+    }
+
+    public Schema Add(Schema Entity)
+    {
+        //throw new NotImplementedException();
+
+        Schema results = repository.Add(Entity);
+        repository.SaveAll();
+
+        return results;
+    }
+
+    public void Delete(long EntityId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Schema Edit(Schema Entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Schema Find(long EntityId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Schema> List()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveAll()
+    {
+        throw new NotImplementedException();
+    }
+}
+
